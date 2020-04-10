cda75bd1b9924ef0542e74bb3ec3735414a0ad6b
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-04-10 21:20:50 +0530

    Components Part 2

--- Front-End Web Development with React/Week1/confusion/src/App.js
+++ Front-End Web Development with React/Week1/confusion/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 // import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -13,9 +22,10 @@ function App() {
         </NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
