33c388d55fa1e775fd5fa9dcc960c1963dc59323
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-04-19 20:45:07 +0530

    Presentational and Container Components

---
+++ Front-End Web Development with React/Week2/confusion/src/components/DishdetailComponent.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+
+export default class DishDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    //----------Removed due to anti-pattern of updating state as props change.---------
+    //--------- Start article: https://stackoverflow.com/a/32414771/8217589 ------------------
+    //---------- Base Resource: https://reactjs.org/docs/react-component.html#static-getderivedstatefromprops -------
+    //----------https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html -------------
+    //    this.state = {
+    //      dishDetails: this.props.dish,
+    //    };
+  }
+
+  //------Implemented using resource: https://reactjs.org/docs/react-component.html#componentdidupdate
+
+  // Anti-pattern Should avoid for this simple use case------------------
+  //   componentDidUpdate(prevProps) {
+  //     if (this.props.dish != prevProps.dish) {
+  //       this.setState({ dishDetails: this.props.dish });
+  //     }
+  //   }
+
+  renderDish(dish) {
+    if (dish) {
+      return (
+        <Card>
+          <CardImg top src={this.props.dish.image} alt={this.props.dish.name} />
+          <CardBody>
+            <CardTitle> {this.props.dish.name} </CardTitle>
+            <CardText> {this.props.dish.description} </CardText>
+          </CardBody>
+        </Card>
+      );
+    }
+  }
+
+  renderComments(comments) {
+    if (comments) {
+      return (
+        <ul>
+          {comments.map((comment) => {
+            return (
+              <li className="list-unstyled" key={comment.id}>
+                <div className="row">
+                  <div className="col-12">
+                    <h4>{comment.comment}</h4>
+                  </div>
+                  <div className="col-12">
+                    -- {`${comment.author}, ${new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}`}
+                  </div>
+                </div>
+                <br />
+                <br />
+              </li>
+            );
+          })}
+        </ul>
+      );
+    } else {
+      return (<div></div>);
+    }
+  }
+
+  render() {
+    //   console.log(this.state.dishDetails);
+    if(this.props.dish) {
+    return (
+      <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          {/* <Card>
+                   <CardImg
+                     top
+                     src={this.props.dish.image}
+                     //src={this.state.dishDetails.image} -----------> Anti-pattern
+                     alt={this.props.dish.name}
+                     //alt={this.state.dishDetails.name}  -----------> Anti-pattern
+                   />
+                   <CardBody>
+                     <CardTitle> {this.props.dish.name} </CardTitle>
+                     <CardTitle> {this.state.dishDetails.name} </CardTitle>  -----------> Anti-pattern
+                     <CardText> {this.props.dish.description} </CardText>
+                     <CardText> {this.state.dishDetails.description} </CardText> -----------> Anti-pattern
+                   </CardBody>
+                 </Card> */}
+
+          {this.renderDish(this.props.dish)}
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          {this.renderComments(this.props.dish.comments)}
+        </div>
+      </div>
+      </div>
+    );
+                } else {
+                  return(<div></div>)
+                }
+  }
+}
---
+++ Front-End Web Development with React/Week2/confusion/src/components/MenuComponent.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+// import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+
+class Menu extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  onDishSelect(dish) {
+    this.setState({
+      selectedDish: dish,
+    });
+  }
+
+
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div key={dish.id} className="col-12 col-md-5 m-1">
+          <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />{" "}
+            <CardImgOverlay>
+              <CardTitle> {dish.name} </CardTitle>{" "}
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
+
+    return (
+      <div className="container">
+        <div className="row"> {menu} </div>
+      </div>
+    );
+  }
+}
+
+export default Menu;
