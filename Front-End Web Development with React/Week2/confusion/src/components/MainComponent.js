c5da39f9ca6859470e452594d0e65bf639dec372
OMKAR AGRAWAL <Omkaragrawal@users.noreply.github.com>
2020-04-23 03:25:06 +0530

    Single Page Applications Part 1

--- Front-End Web Development with React/Week2/confusion/src/components/MainComponent.js
+++ Front-End Web Development with React/Week2/confusion/src/components/MainComponent.js
@@ -5,15 +5,22 @@ import Header from "./HeaderComponent";
 import Home from './HomeComponent';
 import Menu from "./MenuComponent";
 // import DishDetail from "./DishdetailComponent";
+import Contact from './ContactComponent';
 import Footer from "./FooterComponent";
 import { DISHES } from "../shared/dishes";
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -26,6 +33,9 @@ class Main extends Component {
     const HomePage = () => {
       return (
         <Home
+          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+          leader={this.state.leaders.filter(leader => leader.featured)[0]}
         />
       );
     }
@@ -48,6 +58,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />} />
           <Redirect to="/home" />
         </Switch>
         <Footer />
